d462e8e76e25b4de4257c6b23713e875366d5124
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2025-12-21 16:37:47 +0000

    all features now functional and all known bugs fixed

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// TODO
-// inverse appears to be messing with decimal point numbers
-
 document.addEventListener("DOMContentLoaded", function() {
     // Constants
     const MAX_NUM = 999999999999999;
@@ -13,14 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const operation = {
         num1: null,
         num2: null,
+        num2Temp: null,
         operator: null,
         result: null,
+        miniDisplayNum1: null,
+        miniDisplayNum2: null,
+        miniDisplayOperator: null,
     };
 
     const calculatorState = {
         operatorBtnClicked: false,
         equalsBtnClicked: false,
+        clearBtnClicked: false,
+        inverseBtnClicked: false,
+        deleteLastNumberBtnClicked: false,
         dividedByZero: false,
+        operatorChangedAfterEquals: false,
+        justClearedAfterEquals: false,
     };
 
     // DOM References
@@ -36,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const miniDisplayNum1 = document.getElementById("mini-display-number-one");
     const miniDisplayOperator = document.getElementById("mini-display-operator");
     const miniDisplayNum2 = document.getElementById("mini-display-number-two");
+    const miniDisplayOperatorTemp = document.getElementById("mini-display-operator-temp");
 
     // Event Listeners
     document.body.addEventListener("keydown", handleKeyboardPress);
@@ -54,20 +61,12 @@ document.addEventListener("DOMContentLoaded", function() {
     );
 
     // Functions & Core Logic
-    function clearDisplay() {            
-        if (!isInitialState()) {
-            assignNumBasedOnState();
-        }
-
+    function clearDisplay() {    
         display.textContent = "0";
+        calculatorState.clearBtnClicked = true;
     };
 
     function clearAll() {
-        display.textContent = "0";
-        miniDisplayNum1.textContent = "";
-        miniDisplayNum2.textContent = "";
-        miniDisplayOperator.textContent = "";
-
         for (property in operation) {
             operation[property] = null;
         }
@@ -75,11 +74,18 @@ document.addEventListener("DOMContentLoaded", function() {
         for (property in calculatorState) {
             calculatorState[property] = false;
         }
+
+        display.textContent = "0";
+        miniDisplayNum1.textContent = "";
+        miniDisplayOperator.textContent = "";
+        miniDisplayNum2.textContent = "";
+        miniDisplayOperatorTemp.textContent = "";
     };
 
     function inverseNumber() {
         let number = display.textContent;
         number = stripCommas(number);
+        const decimalPlaces = getDecimalPlaces(number);
         number = parseFloat(number);
 
         if (number > 0) {
@@ -88,12 +94,11 @@ document.addEventListener("DOMContentLoaded", function() {
             number = number * -1;
         }
 
-        assignNumBasedOnState();
-
-        operation.result = null;
-
         display.textContent = number.toLocaleString(undefined, { 
+            minimumFractionDigits: decimalPlaces,
             maximumFractionDigits: MAX_DECIMALS });
+
+        calculatorState.inverseBtnClicked = true;
     };
 
     function deleteLastNumber() {
@@ -104,14 +109,12 @@ document.addEventListener("DOMContentLoaded", function() {
         number = parseFloat(number);
 
         if (number === "" || isNaN(number)) number = 0;
-        
-        if (!isInitialState()) {
-            assignNumBasedOnState();
-        }
 
         display.textContent = number.toLocaleString(undefined, {
             minimumFractionDigits: decimalPlaces,
             maximumFractionDigits: MAX_DECIMALS });
+            
+        calculatorState.deleteLastNumberBtnClicked = true;
     };
 
     function handleNumberClick(e) {
@@ -120,47 +123,17 @@ document.addEventListener("DOMContentLoaded", function() {
             calculatorState.dividedByZero = false;
         }
 
+        if (calculatorState.deleteLastNumberBtnClicked) calculatorState.deleteLastNumberBtnClicked = false;
+
         updateDisplay(e.target.textContent);
     };
 
-    function handleOperatorClick(e) {
-        if (!calculatorState.operatorBtnClicked) storeNumber(display.textContent);
-
-        if (calculatorState.equalsBtnClicked && e.target.textContent != "=") {
-            operation.num2 = null;
-            calculatorState.equalsBtnClicked = false;
-        }
-        
-        updateMiniDisplay();
-        
-        performOperationChecks(e);
-
-        if (e.target.textContent != "=") {
-            storeOperator(e.target.textContent);
-            updateMiniDisplay();
-        }
-        
-        calculatorState.operatorBtnClicked = true;
-    };
-
-    function handleKeyboardPress(e) {
-        numpad.forEach(btn => {
-            if (!btn.disabled) {
-                if (e.key.toLowerCase() === btn.dataset.keyboard.toLowerCase()) {
-                    btn.classList.add('active');
-                    btn.click();
-                    setTimeout(() => btn.classList.remove('active'), BUTTON_TIMEOUT);
-                }
-            }
-        });
-    }
-
-    function updateDisplay(input) {
-        if (display.textContent.includes(".") && input === ".") return;
-
+    function updateDisplay(input) {        
         if (calculatorState.operatorBtnClicked) clearDisplayOnNewNum();
-        
+    
         if (calculatorState.equalsBtnClicked) clearDisplayOnEqualsBtnClicked();
+
+        if (display.textContent.includes(".") && input === ".") return;
 
         const cleanedNumber = stripCommas(display.textContent);
         const decimalPlaces = getDecimalPlaces(cleanedNumber);
@@ -183,6 +156,79 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
 
+    function clearDisplayOnNewNum() {
+        display.textContent = "";
+        calculatorState.operatorBtnClicked = false;
+    };
+
+    function clearDisplayOnEqualsBtnClicked() {
+        let currentDisplayValue = stripCommas(display.textContent);
+        currentDisplayValue = parseFloat(currentDisplayValue);
+
+        operation.num1 = null;
+        operation.result = null;
+
+        if (operation.num2Temp !== null && operation.operator !== null) {
+            operation.num2 = operation.num2Temp;
+        }
+        
+        display.textContent = "";
+        miniDisplayNum1.textContent = "";
+        miniDisplayNum2.textContent = "";
+        miniDisplayOperator.textContent = "";
+        miniDisplayOperatorTemp.textContent = "";
+        operation.miniDisplayNum1 = null;
+        operation.miniDisplayNum2 = null; 
+        operation.miniDisplayOperator = null;
+        calculatorState.equalsBtnClicked = false;
+        calculatorState.justClearedAfterEquals = true;
+    };
+
+    function handleOperatorClick(e) {
+        if (!calculatorState.operatorBtnClicked && !calculatorState.equalsBtnClicked) {
+            storeNumber(display.textContent);
+        }
+
+        if (calculatorState.clearBtnClicked || calculatorState.inverseBtnClicked || calculatorState.deleteLastNumberBtnClicked) {            
+            assignNumBasedOnState(display.textContent);
+            
+            if (calculatorState.inverseBtnClicked) calculatorState.inverseBtnClicked = false;
+            if (calculatorState.deleteLastNumberBtnClicked) calculatorState.deleteLastNumberBtnClicked = false;
+            if (calculatorState.clearBtnClicked) calculatorState.clearBtnClicked = false;
+        }
+        
+        if (calculatorState.justClearedAfterEquals && e.target.textContent != "=") {
+            resetOperationWithCurrentDisplay(display.textContent);
+            storeOperator(e.target.textContent);
+
+            calculatorState.justClearedAfterEquals = false;
+            
+            updateMiniDisplay();
+            return;
+        }
+        
+        if (calculatorState.equalsBtnClicked && e.target.textContent != "=") {
+            resetOperationWithCurrentDisplay(display.textContent);
+            storeOperator(e.target.textContent);
+
+            calculatorState.equalsBtnClicked = false;
+
+            updateMiniDisplay();
+            return;
+        }
+
+        performOperationChecks(e);
+
+        if (e.target.textContent != "=") {
+            storeOperator(e.target.textContent);
+            calculatorState.operatorBtnClicked = true;
+        } else {
+            calculatorState.equalsBtnClicked = true;
+        }
+        
+        updateMiniDisplay();
+    };
+
     function storeNumber(number) {
         number = stripCommas(number);
         number = parseFloat(number);
@@ -192,6 +238,78 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             operation.num2 = number;
         }
+    };
+
+    function assignNumBasedOnState(number) {
+        number = stripCommas(number);
+        number = parseFloat(number);
+        
+        if (operation.operator === null || calculatorState.equalsBtnClicked) {
+            operation.num1 = number;
+
+        } else if (operation.operator !== null && !calculatorState.operatorBtnClicked) {
+            operation.num2 = number;
+
+        }
+    };
+
+    function resetOperationWithCurrentDisplay(number) {
+        let number = stripCommas(number);
+        number = parseFloat(number);
+        operation.num1 = number;
+        
+        operation.num2 = null;
+        operation.num2Temp = null;
+        operation.result = null;
+        operation.miniDisplayNum1 = null;
+        operation.miniDisplayNum2 = null;
+        operation.miniDisplayOperator = null;
+        calculatorState.operatorBtnClicked = true;
+    };
+
+    function storeOperator(operator) {           
+        if (!calculatorState.dividedByZero) {
+            operation.operator = operator;
+        }
+    };
+
+    function updateMiniDisplay() {  
+        if (operation.miniDisplayNum1 === null) {
+            miniDisplayNum1.textContent = operation.num1;
+        } else {
+            miniDisplayNum1.textContent = operation.miniDisplayNum1;
+        }
+
+        if (operation.miniDisplayOperator === null) {
+            miniDisplayOperator.textContent = operation.operator;
+        } else {
+            miniDisplayOperator.textContent = operation.miniDisplayOperator;
+        }
+
+        if (operation.miniDisplayNum2 === null) {
+            miniDisplayNum2.textContent = operation.num2;
+        } else {
+            miniDisplayNum2.textContent = operation.miniDisplayNum2;
+        }
+
+        if (calculatorState.equalsBtnClicked) {
+            miniDisplayOperatorTemp.textContent = "";
+        } else if (operation.result != null) {
+            miniDisplayOperatorTemp.textContent = operation.operator;
+        }
+    };
+
+    function handleKeyboardPress(e) {
+        numpad.forEach(btn => {
+            if (!btn.disabled) {
+                if (e.key.toLowerCase() === btn.dataset.keyboard.toLowerCase()) {
+                    btn.classList.add('active');
+                    btn.click();
+                    setTimeout(() => btn.classList.remove('active'), BUTTON_TIMEOUT);
+                    console.log(e.key)
+                }
+            }
+        });
     };
 
     function performOperationChecks(e) {
@@ -206,22 +324,6 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 calculatorState.equalsBtnClicked = true;
             }
-        }
-    };
-
-    function storeOperator(operator) {           
-        if (!calculatorState.dividedByZero) {
-            operation.operator = operator;
-        }
-    };
-
-    function updateMiniDisplay() {        
-        if (operation.num1 != null) miniDisplayNum1.textContent = operation.num1;
-        if (operation.operator != null) miniDisplayOperator.textContent = operation.operator;
-
-        if (operation.result != null) {
-            miniDisplayNum1.textContent = operation.result;
-            miniDisplayNum2.textContent = "";
         }
     };
 
@@ -243,65 +345,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 break;
         }
 
-        operation.result = parseFloat(operation.result.toFixed(MAX_DECIMALS))
+        operation.result = parseFloat(operation.result.toFixed(MAX_DECIMALS));
         operation.result = checkResultLength(operation.result);
+        operation.miniDisplayNum1 = operation.num1;
+        operation.miniDisplayNum2 = operation.num2;
+        operation.miniDisplayOperator = operation.operator;
         operation.num1 = operation.result;
-
-        miniDisplayNum1.textContent = operand1;
-        miniDisplayOperator.textContent = operator;
-        miniDisplayNum2.textContent = operand2;
-
+        operation.num2Temp = operation.num2;
+    
         display.textContent = operation.result.toLocaleString(undefined, { 
             maximumFractionDigits: MAX_DECIMALS });
-    };
-
-    // Helper Functions
-    function isInitialState() {
-        return operation.num1 === null && operation.num2 === null && operation.operator === null;
-    }
-
-    function assignNumBasedOnState() {
-        if (operation.operator === null || calculatorState.equalsBtnClicked) {
-            operation.num1 = 0;
-        } else if (operation.operator !== null && !calculatorState.operatorBtnClicked) {
-            operation.num2 = 0;
-        }
-    };
-
-    function clearDisplayOnNewNum() {
-        display.textContent = "";
-        calculatorState.operatorBtnClicked = false;
-    };
-
-    function clearDisplayOnEqualsBtnClicked() {
-        operation.num1 = null;
-        miniDisplayNum1.textContent = "";
-        miniDisplayNum2.textContent = "";
-        miniDisplayOperator.textContent = "";
-        calculatorState.equalsBtnClicked = false;
-    };
-
-    function stripCommas(number) {
-        return number
-            .toString()
-            .split("")
-            .filter(char => {return /[0-9\.\-]/
-            .test(char)})
-            .join("");
-    };
-
-    function getDecimalPlaces(number) {
-        return number.split(".")[1] ? number.split(".")[1].length : 0;
-    };
-
-    function flipDisabledStatus() {
-        operatorBtns.forEach(btn => {
-            btn.disabled = !btn.disabled;
-        });
-
-        calculatorBtns.forEach(btn => {
-            btn.disabled = !btn.disabled;
-        });
     };
 
     function isDividedByZero(number, operator) {
@@ -312,10 +365,10 @@ document.addEventListener("DOMContentLoaded", function() {
             display.textContent = ">:[";
             calculatorState.dividedByZero = true;
             return true;
-        }
+        };
         return false;
     };
-
+    
     function add(num1, num2) {
         return num1 + num2;
     };
@@ -339,5 +392,29 @@ document.addEventListener("DOMContentLoaded", function() {
             return number = MIN_NUM;
         }
         return number;
+    };
+
+    // Helper Functions
+    function stripCommas(number) {
+        return number
+            .toString()
+            .split("")
+            .filter(char => {return /[0-9\.\-]/
+            .test(char)})
+            .join("");
+    };
+
+    function getDecimalPlaces(number) {
+        return number.split(".")[1] ? number.split(".")[1].length : 0;
+    };
+
+    function flipDisabledStatus() {
+        operatorBtns.forEach(btn => {
+            btn.disabled = !btn.disabled;
+        });
+
+        calculatorBtns.forEach(btn => {
+            btn.disabled = !btn.disabled;
+        });
     };
 });
